7ef47bc4b507feedbfe0421f3bc36e980a150f04
Solace <monkdev9@gmail.com>
2026-02-28 12:09:20 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['res.cloudinary.com'],
   },
-  // Enable TypeScript and ESLint options via default settings
   eslint: {
-    // Warning only; don't fail build on lint errors
     ignoreDuringBuilds: true,
   },
   output: 'standalone',
